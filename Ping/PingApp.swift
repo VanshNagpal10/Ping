e678532845371a88a6907374a91f412ebfdaa6c1
Vansh Nagpal <vanshnagpal21@gmail.com>
2026-01-06 13:00:04 +0530

    Initial Commit

---
+++ Ping/PingApp.swift
@@ -0,0 +1,17 @@
+//
+//  PingApp.swift
+//  Ping
+//
+//  Created by Vansh Nagpal on 06/01/26.
+//
+
+import SwiftUI
+
+@main
+struct PingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
