61dd2e41de65bf7129402aba0fd53a1308997585
Vansh Nagpal <vanshnagpal21@gmail.com>
2026-02-17 19:36:32 +0530

    scene changes

--- Ping/PingApp.swift
+++ Ping/PingApp.swift
@@ -9,17 +9,34 @@ import SwiftUI
 
 @main
 struct PingApp: App {
+    #if os(iOS)
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    #endif
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .preferredColorScheme(.dark)
+                #if os(iOS)
+                .onAppear {
+                    // Request landscape on first window scene
+                    guard let windowScene = UIApplication.shared.connectedScenes
+                        .compactMap({ $0 as? UIWindowScene }).first else { return }
+                    windowScene.requestGeometryUpdate(.iOS(interfaceOrientations: .landscape))
+                }
+                #endif
         }
     }
-    
-    init() {
-        // Lock to landscape orientation for iPad
-        #if os(iOS)
-        UIDevice.current.setValue(UIInterfaceOrientation.landscapeRight.rawValue, forKey: "orientation")
-        #endif
+}
+
+#if os(iOS)
+/// Locks the app to landscape orientations via the supported orientations mask.
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        supportedInterfaceOrientationsFor window: UIWindow?
+    ) -> UIInterfaceOrientationMask {
+        return .landscape
     }
 }
+#endif
